0d3b5482b10d973121622323647142c8019360cd
hamza <hamza.rebb@gmail.com>
2022-01-24 09:28:37 +0100

    réalisation script sql

--- sql/select_jrpg_single.sql
+++ sql/select_jrpg_single.sql
@@ -1,18 +1,23 @@
 select nom_jeu
 from nom_jrpg
+where nom_jeu = @nom
 
 select date_sortie
 from date_jrpg
+where date_sortie = @date
 
 select console_jeu
 from console_jrpg
+where console_jeu = @console
 
 select franchise_jeu
 from serie_jrpg
+where franchise_jeu = @editeur
 
 select studio_editeur
 from editeur_jrpg
+where studio_editeur = @editeur
 
 select adaptation_jeu
 from adaptation_jrpg
-
+where adaptation_jeu = @adaptation
---
+++ sql/update_jrpg.sql
@@ -0,0 +1,17 @@
+update nom_jrpg
+set nom_jeu = @nom
+
+update date_jrpg
+set date_sortie = @date
+
+update console_jrpg
+set console_jeu = @console
+
+update serie_jrpg
+set franchise_jeu = @serie
+
+update editeur_jrpg 
+set studio_editeur = @editeur
+
+update adaptation_jrpg
+set adaptation_jeu = @adaptation
