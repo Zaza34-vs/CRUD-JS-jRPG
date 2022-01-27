ed1003161d192fd46d19bb2bb2e07da532ad99eb
hamza <zazarebba975@gmail.com>
2022-01-27 16:20:49 +0100

    modification sur la base de donnée

--- sql/jrpg.sql
+++ sql/jrpg.sql
@@ -1,19 +1,30 @@
 #------------------------------------------------------------
-#        Script MySQL.
+#        Script
+MySQL.
 #------------------------------------------------------------
 
 
 #------------------------------------------------------------
-# Table: nom_jrpg
+#
+Table:
+nom_jrpg
 #------------------------------------------------------------
 
-CREATE TABLE jrpg(
-        id_jrpg     Int  Auto_increment  NOT NULL ,
-        nom_jeu         Char (5) NOT NULL ,
+CREATE TABLE jrpg
+(
+        id_jrpg Int
+        Auto_increment  NOT NULL ,
+        nom_jeu         Char
+        (100) NOT NULL ,
         date_sortie     Date NOT NULL ,
-        editeur_jrpg    Char (100) NOT NULL ,
-        console_jrpg    Varchar (100) NOT NULL ,
-        adaptation_jrpg Char (5) NOT NULL ,
-        franchise_jrpg  Char (5) NOT NULL
-	,CONSTRAINT jrpg_PK PRIMARY KEY (id_jrpg)
+        editeur_jrpg    Char
+        (100) NOT NULL ,
+        console_jrpg    Varchar
+        (100) NOT NULL ,
+        adaptation_jrpg Char
+        (100) NOT NULL ,
+        franchise_jrpg  Char
+        (100) NOT NULL
+	,CONSTRAINT jrpg_PK PRIMARY KEY
+        (id_jrpg)
 )ENGINE=InnoDB;
