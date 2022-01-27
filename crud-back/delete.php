3eea6e6c2c32fb1eb93144e949ce402394470db9
hamza <zazarebba975@gmail.com>
2022-01-27 15:47:54 +0100

    update delete.php

--- crud-back/delete.php
+++ crud-back/delete.php
@@ -2,8 +2,8 @@
 
     function getAllResultats($numero_connexion, $GLOBALS_INI, $VARS_HTML){
         $spathSQL = $GLOBALS_INI["PATH_HOME"] . $GLOBALS_INI["PATH_SQL"] . "delete_jrpg.sql";
-        $resultat= treatDatas($numero_de_connexion, $spathSQL, array(
-            "id_jrpg" => $VARS_HTML["id_jrpg"]
+        $resultat= treatDatas($numero_connexion, $spathSQL, array(
+                                                                "id_jrpg" => $VARS_HTML["id_jrpg"]
             ));
     }
 ?>
